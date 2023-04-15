289e5a8c5647e62ae5c9c9b021c83d8c05675228
Vitalii Zaliva <neapolvet@gmail.com>
2023-04-15 14:53:05 +0300

    Finish basic layout for header

--- dist/global.6825d2fa.js
+++ dist/global.6825d2fa.js
@@ -6977,7 +6977,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55307" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64535" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
